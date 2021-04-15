0b663385826287c2de212dbe8e79f3a16f736a7f
Julio Rosario <rosariojulio40@gmail.com>
2021-04-15 11:20:39 -0400

    Change order of projects

--- src/app/project-page/project-page.component.ts
+++ src/app/project-page/project-page.component.ts
@@ -8,20 +8,26 @@ import { ProjectModel } from '../model/project.model';
 export class ProjectPageComponent implements OnInit {
 
   projectModelArray:ProjectModel[] = [
+
+
+    new ProjectModel({
+      title:"Crypto Tracker",
+      imgFileArray:[ "../../assets/images/cryptotracker/cryptotrackerList.png",
+      "../../assets/images/cryptotracker/cryptotrackerGraph.png",
+      "../../assets/images/cryptotracker/cryptotrackerNews.png",
+      "../../assets/images/cryptotracker/cryptotrackerf+Favorites.png"
+
+    ],
+      description:"An iOS app that allow users to track their favorite cryptocurrencies in real time. It uses APIs like "
+      + "SwiftJSON, Alamofire, and Charts API. ",
+      link:"http://juliocr23.github.io/CryptocurrenciesWeb/"
+    }),
     new ProjectModel({
       title:"Alien Invasion",
       imgFileArray:["../../assets/images/AlienInvasion.gif"],
       description: "  A scroller  video game, created using Java.  Base on a kid trying to save the world from an alien invasion.",
       link:"https://github.com/juliocr23/AlienInvasio"
     }),
-
-    new ProjectModel({
-      title:"Zero Fighter",
-      imgFileArray:["../../assets/images/jmpShowCase.gif"],
-      description: "  A desktop video game in 2D, created using Java from scratch. The type of game you would play on a retro console.",
-      link:"https://github.com/juliocr23/JMP"
-    }),
-
     new ProjectModel({
       title:"Password Wallet",
       imgFileArray:["../../assets/images/passwordpocket/PasswordPocketLogin.png",
@@ -36,17 +42,12 @@ export class ProjectPageComponent implements OnInit {
 
       link:"https://juliocr23.github.io/PasswordPocket/"
     }),
-    new ProjectModel({
-      title:"Crypto Tracker",
-      imgFileArray:[ "../../assets/images/cryptotracker/cryptotrackerList.png",
-      "../../assets/images/cryptotracker/cryptotrackerGraph.png",
-      "../../assets/images/cryptotracker/cryptotrackerNews.png",
-      "../../assets/images/cryptotracker/cryptotrackerf+Favorites.png"
 
-    ],
-      description:"An iOS app that allow users to track their favorite cryptocurrencies in real time. It uses APIs like "
-      + "SwiftJSON, Alamofire, and Charts API. ",
-      link:"http://juliocr23.github.io/CryptocurrenciesWeb/"
+    new ProjectModel({
+      title:"Zero Fighter",
+      imgFileArray:["../../assets/images/jmpShowCase.gif"],
+      description: "  A desktop video game in 2D, created using Java from scratch. The type of game you would play on a retro console.",
+      link:"https://github.com/juliocr23/JMP"
     })
   ];
   constructor() { }
